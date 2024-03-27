d6f4bbbd0eb72a6f4ec293e9dc73109b21aaf6e2
Ibrahim Farooq <ifarooq9@gmail.com>
2024-03-27 11:36:58 +0430

    Lesson_1_4 assignment complete and submitting for the review

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
-const AddTodoForm = (props) => {
-
-  const [todoTitle, setTodoTitle] = useState('');
+const AddTodoForm = ({ onAddTodo }) => {
+  const [todoTitle, setTodoTitle] = useState("");
 
   const handleTitleChange = (event) => {
     const newTodoTitle = event.target.value;
     setTodoTitle(newTodoTitle);
-  }
+  };
 
   const handleAddTodo = (event) => {
     event.preventDefault();
@@ -16,23 +15,30 @@ const AddTodoForm = (props) => {
 
     const newTodo = {
       title: todoTitle,
-      id: Date.now()
-    }
+      id: Date.now(),
+    };
 
-    props.onAddTodo(newTodo);
+    onAddTodo(newTodo);
 
-    setTodoTitle('');
-
-  }
+    setTodoTitle("");
+  };
   return (
     <div>
       <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Title: </label>
-        <input id="todoTitle" type="text" name="title" value={todoTitle} onChange={handleTitleChange}/>
-        <button type="submit" style={{ marginLeft: "5px"}}>Add</button>
+        <input
+          id="todoTitle"
+          type="text"
+          name="title"
+          value={todoTitle}
+          onChange={handleTitleChange}
+        />
+        <button type="submit" style={{ marginLeft: "5px" }}>
+          Add
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddTodoForm
+export default AddTodoForm;
