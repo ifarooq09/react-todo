7e50c00baa1b51e6a985df69536008c973cd6595
Ibrahim Farooq <ifarooq9@gmail.com>
2024-04-15 14:06:16 +0430

    Lesson 1.7 assignment completed and submitted for review

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,34 @@ import { useEffect, useState } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
-const useSemiPersistentState = () => {
+function App() {
+
   const [todoList, setTodoList] = useState(() => {
     const savedTodoList = localStorage.getItem("savedTodoList");
     return savedTodoList ? JSON.parse(savedTodoList) : [];
   });
 
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    const myPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const data = {
+          todoList: todoList
+        }
+        resolve(data)
+      }, 2000)
+    }).then((result) => {
+      setTodoList(result.todoList)
+      setIsLoading(false)
+    })
+  }, [])
+
+  useEffect(() => {
+    if(!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
   }, [todoList]);
-
-  return [todoList, setTodoList];
-
-};
-
-
-function App() {
-
-  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
@@ -36,8 +46,12 @@ function App() {
       <h1>Todo List</h1>
 
       <AddTodoForm onAddTodo={addTodo} />
-
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {
+        isLoading ? (
+        <p>Loading...</p>
+        ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        )}
     </>
   );
 }
